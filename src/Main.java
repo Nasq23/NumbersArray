44e70e6f854893a25dc4d0fd4a8227a93274f3bc
GBTC441009UR <GBTC441009UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-11 16:30:29 -0400

    numbers array

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String [] args){
+
+
+        for(int row=0; row<=9; row++) {
+
+            System.out.print("row" + row);
+
+            for (int col=0; col<=4; col++) {
+
+                System.out.print("col" + col);
+
+            }
+
+            System.out.println();
+        }
+    }
+}
